62ff5fd0e29336a2d1e066abdd94a7498536542f
Alfred Bez <abez@Alfreds-iMac.local>
2014-07-04 10:02:35 +0200

    works now in ST3

--- Unplash.py
+++ Unplash.py
@@ -1,35 +1,35 @@
 import sublime, sublime_plugin, random, os
 
-def insert_unsplash( kind ):
-    from os.path import join
-    package_path = join(sublime.packages_path(), "Unsplash")
-    unpath = os.path.join(package_path, "unsplash.txt")
-
-    with open(unpath) as f:
-        lines = f.read().splitlines()
-        splash = random.choice(lines)
-
-    if kind == "tag":
-        imagetag = '<img src="%s">'% (splash)
-    elif kind == "background":
-        imagetag = 'background-image: url("%s") no-repeat center center'% (splash)
-    else:
-        imagetag = '%s'% (splash)
-
-    view = sublime.active_window().active_view()
-    edit = view.begin_edit()
-    for region in view.sel():
-        view.replace(edit, region, imagetag)
-    view.end_edit(edit)
-
 class InsertUnsplashCommand(sublime_plugin.TextCommand):
+    def run(self, edit, args):
+        from os.path import join
+        package_path = join(sublime.packages_path(), "Unsplash")
+        unpath = os.path.join(package_path, "unsplash.txt")
+
+        with open(unpath) as f:
+            lines = f.read().splitlines()
+            splash = random.choice(lines)
+
+        if args['type'] == "tag":
+            imagetag = '<img src="%s">'% (splash)
+        elif args['type'] == "background":
+            imagetag = 'background-image: url("%s") no-repeat center center'% (splash)
+        else:
+            imagetag = '%s'% (splash)
+
+        for region in self.view.sel():
+            self.view.replace(edit, region, imagetag)
+
+class InsertUnsplashTagCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        insert_unsplash('tag')
+        self.view.run_command("insert_unsplash", {"args":{'type':'tag'}})
+
 
 class InsertUnsplashUrlCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        insert_unsplash('url')
+        self.view.run_command("insert_unsplash", {"args":{'type':'url'}})
+
 
 class InsertUnsplashBackgroundCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        insert_unsplash('background')
+        self.view.run_command("insert_unsplash", {"args":{'type':'background'}})
