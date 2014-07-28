f1d2e5e396d9451815acd5b63c828b51a78b564b
Urban Sanden <hej@urre.me>
2014-07-28 11:01:05 +0200

    Fetch images directly via the Tumblr API. Check Sublime versions and use ST2/ST3 urlopen/urllib.request libs accordingly

--- Unplash.py
+++ Unplash.py
@@ -1,19 +1,48 @@
-import sublime, sublime_plugin, random, os
+import sublime, sublime_plugin, random, os, json
+import xml.etree.ElementTree as ET
 
 class InsertUnsplashCommand(sublime_plugin.TextCommand):
     def run(self, edit, args):
-        from os.path import join
-        package_path = join(sublime.packages_path(), "Unsplash")
-        unpath = os.path.join(package_path, "unsplash.txt")
 
-        with open(unpath) as f:
-            lines = f.read().splitlines()
-            splash = random.choice(lines)
+        sublimeVersion = int(sublime.version())
+        st2 = False
+        st3 = False
+
+        if sublimeVersion < 3000:
+            st2 = True
+            from urllib import urlopen
+        else:
+            st3 = True
+            import urllib.request
+
+        tumblr_name = 'unsplash'
+        api_endpoint = 'http://unsplash.tumblr.com/api/read?num=100?type=photo'
+        photoUrls = []
+        settings = sublime.load_settings("Unsplash.sublime-settings")
+        imgWidth = settings.get("imgWidth", 10)
+
+        if st2:
+            resp = urlopen(api_endpoint)
+        else:
+            resp = urllib.request.urlopen(api_endpoint)
+
+        content = resp.read()
+        tree = ET.fromstring(content)
+        post_tags = tree.findall(".//post")
+        post_count = len(post_tags)
+        for post_tag in post_tags:
+
+            for photo_tag in post_tag.findall(".//photo-url"):
+                if photo_tag.attrib['max-width'] == str(imgWidth):
+                    photoUrl = photo_tag.text
+                    photoUrls.append(photoUrl)
+
+        splash = random.choice(photoUrls)
 
         if args['type'] == "tag":
             imagetag = '<img src="%s">'% (splash)
         elif args['type'] == "background":
-            imagetag = 'background-image: url("%s") no-repeat center center'% (splash)
+            imagetag = 'background: url("%s") no-repeat center center'% (splash)
         else:
             imagetag = '%s'% (splash)
 
