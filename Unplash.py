1cec9b6f2ff4d827b0a17e35980e21c0358c1aaf
Urban Sanden <hej@urre.me>
2015-03-13 16:47:34 +0100

    Consistent use of quotes

--- Unplash.py
+++ Unplash.py
@@ -18,8 +18,8 @@ class InsertUnsplashCommand(sublime_plugin.TextCommand):
         tumblr_name = 'unsplash'
         api_endpoint = 'http://unsplash.tumblr.com/api/read?num=100?type=photo'
         photoUrls = []
-        settings = sublime.load_settings("Unsplash.sublime-settings")
-        imgWidth = settings.get("imgWidth", 10)
+        settings = sublime.load_settings('Unsplash.sublime-settings')
+        imgWidth = settings.get('imgWidth', 10)
 
         if st2:
             resp = urlopen(api_endpoint)
@@ -28,22 +28,22 @@ class InsertUnsplashCommand(sublime_plugin.TextCommand):
 
         content = resp.read()
         tree = ET.fromstring(content)
-        post_tags = tree.findall(".//post")
+        post_tags = tree.findall('.//post')
         post_count = len(post_tags)
         for post_tag in post_tags:
 
-            for photo_tag in post_tag.findall(".//photo-url"):
+            for photo_tag in post_tag.findall('.//photo-url'):
                 if photo_tag.attrib['max-width'] == str(imgWidth):
                     photoUrl = photo_tag.text
                     photoUrls.append(photoUrl)
 
         splash = random.choice(photoUrls)
 
-        if args['type'] == "tag":
+        if args['type'] == 'tag':
             imagetag = '<img src="%s">'% (splash)
-        elif args['type'] == "background":
+        elif args['type'] == 'background':
             imagetag = 'background: url("%s") no-repeat center center'% (splash)
-        elif args['type'] == "markdown":
+        elif args['type'] == 'markdown':
             imagetag = '![](%s)'% (splash)
         else:
             imagetag = '%s'% (splash)
@@ -53,16 +53,16 @@ class InsertUnsplashCommand(sublime_plugin.TextCommand):
 
 class InsertUnsplashTagCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.run_command("insert_unsplash", {"args":{'type':'tag'}})
+        self.view.run_command('insert_unsplash', {'args':{'type':'tag'}})
 
 class InsertUnsplashUrlCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.run_command("insert_unsplash", {"args":{'type':'url'}})
+        self.view.run_command('insert_unsplash', {'args':{'type':'url'}})
 
 class InsertUnsplashBackgroundCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.run_command("insert_unsplash", {"args":{'type':'background'}})
+        self.view.run_command('insert_unsplash', {'args':{'type':'background'}})
 
 class InsertUnsplashMarkdownCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.run_command("insert_unsplash", {"args":{'type':'markdown'}})
+        self.view.run_command('insert_unsplash', {'args':{'type':'markdown'}})
